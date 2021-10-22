f81b1ba5cdca68d77fe26682acfa6623a462fa40
unknown <kuwar51@yahoo.com>
2021-10-22 21:43:06 +0530

    tooltip fixed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-$(document).ready(function() {
-  $('.minus').click(function () {
-    var $input = $(this).parent().find('.input4');
+$(document).ready(function () {
+  $(".minus").click(function () {
+    var $input = $(this).parent().find(".input4");
     var count = parseInt($input.val()) - 1;
     count = count < 1 ? 1 : count;
     $input.val(count);
     $input.change();
     return false;
   });
-  $('.plus').click(function () {
-    var $input = $(this).parent().find('.input4');
+  $(".plus").click(function () {
+    var $input = $(this).parent().find(".input4");
     $input.val(parseInt($input.val()) + 1);
     $input.change();
     return false;
+  });
+});
+
+// chat section
+
+$(document).ready(function(){
+  $(".chatbtn").click(function(){
+    $(".chatbox").slideToggle('slow');
   });
 });
